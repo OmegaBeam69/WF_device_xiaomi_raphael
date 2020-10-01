bae786a08cec649cb6d0a95a5fa5eb32b815b624
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    raphael: Remove packages at build time

--- device.mk
+++ device.mk
@@ -433,6 +433,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/seccomp/,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy)
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl-xiaomi \
