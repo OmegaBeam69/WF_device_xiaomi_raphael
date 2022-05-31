cd656e8e95cefadd3532dd0d8c324e647ad0805e
TTTT555 <albukerko@gmail.com>
2022-05-31 20:29:16 +0200

    raphael: Include ViPER4Android FX to ROM
    git clone https://github.com/DanipunK1/vendor_v4afx.git -b twelve vendor/v4afx

--- spark_raphael.mk
+++ spark_raphael.mk
@@ -28,6 +28,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # UDFPS animations
 EXTRA_UDFPS_ANIMATIONS := true
 
+$(call inherit-product, vendor/v4afx/config.mk)
+
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
