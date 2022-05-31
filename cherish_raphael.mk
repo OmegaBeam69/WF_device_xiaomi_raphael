5e3abb2e0df13f391d65c62530c7e4acf1abd89b
TTTT555 <albukerko@gmail.com>
2022-05-31 20:29:16 +0200

    raphael: Include ViPER4Android FX to ROM
    git clone https://github.com/DanipunK1/vendor_v4afx.git -b twelve vendor/v4afx
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- cherish_raphael.mk
+++ cherish_raphael.mk
@@ -28,6 +28,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
+#V4A
+$(call inherit-product, vendor/v4afx/config.mk)
+
 # Charging Animation
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
