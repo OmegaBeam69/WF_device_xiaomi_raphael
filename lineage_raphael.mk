6a2d1aacc923030c85f49b9646ab94eb4a851762
Wasik Ahmmed Fahm <62275326+WasikFahim69@users.noreply.github.com>
2022-11-13 20:32:58 +0600

    Ricedroid adaption

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -18,15 +18,46 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 # Inherit some common VoltageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Official-ify
+# chipset flag (one word only/no spacing)
+# friendly tip: builders can use init_xxx.cpp as workaround for spacing
+# e.g. property_override("ro.rice.chipset", "Snapdragon 870 5G");
+RICE_CHIPSET := msmnile
+
+#Official
 RICE_OFFICIAL := false
+
+# maintainer flag (one word only/no spacing)
+# friendly tip: builders can use init_xxx.cpp as workaround for spacing
+# e.g. property_override("ro.rice.maintainer", "get riced");
 RICE_MAINTAINER := Wasik Fahim
 
-# UDFPS animations
-EXTRA_UDFPS_ANIMATIONS := true
+# package type flag (one word only/no spacing)
+# this will reflect on build/display version, a firmware package/zip name 
+# e.g. riceDroid-7.0-COMMUNITY-device-AOSP.zip - AOSP is the default package type, WITH_GMS will override the package type to PIXEL
+RICE_PACKAGE_TYPE := AOSP
 
-# Bootanimation Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# Graphene Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
+
+# Allow usage of custom binary linker (LD), default is false
+TARGET_KERNEL_OPTIONAL_LD := false
+
+# Spoof build description/fingerprint as pixel device
+TARGET_USE_PIXEL_FINGERPRINT := true
+
+# GMS build flags, if none were defined the package build type will be AOSP
+WITH_GMS := true
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_raphael
