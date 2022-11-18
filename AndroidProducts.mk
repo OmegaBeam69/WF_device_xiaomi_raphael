cec47c9fe6726770f7476bd538c837ca23002b9d
unknown <Wasik15-1156@diu.edu.bd>
2022-11-18 10:35:17 +0600

    'Sparkify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_raphael.mk
+    $(LOCAL_DIR)/spark_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_raphael-user \
-    voltage_raphael-userdebug \
-    voltage_raphael-eng
+    spark_raphael-user \
+    spark_raphael-userdebug \
+    spark_raphael-eng
