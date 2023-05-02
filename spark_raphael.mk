8193b6f1948a855b858b7e3a4cac555b96eb04e2
OmegaBeam69 <wasik15-1156@diu.edu.bd>
2023-05-02 19:27:55 +0600

    Get Sparky

--- spark_raphael.mk
+++ spark_raphael.mk
@@ -16,13 +16,14 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Maintainer Flag (one word only/no spacing)
-MAINTAINER := ΛＲＣＨＩＴΣＣＴツ(@SageOfD6Path)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.spark.maintainer= WasikFahim(@OmegaBeam)
 
 # GAPPS build flag, if not defined build type is vanilla
-#SUPERIOR_GAPPS = full
+WITH_GAPPS := false
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -34,21 +35,15 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_ENABLE_BLUR := true
 
 # Officialify 
-SUPERIOR_OFFICIAL := true
-
+SPARK_BUILD_TYPE := UNOFFICIAL
 #Udfps Stuff
 SUPERIOR_UDFPS_ANIMATIONS := true
-
-# Superior Prebuilts
-USE_QUICKPIC := true
-USE_DUCKDUCKGO := true
-USE_MOTO_CALCULATOR := true
 
 # Quick Tap Feature
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_raphael
+PRODUCT_NAME := spark_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9T Pro
